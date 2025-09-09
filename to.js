d1f23355c7634e58933ac976a73f281d8b4dfd93
egypt-academy <egyptacademy11@gmail.com>
2025-09-09 03:18:33 +0300

    Update to.js

--- to.js
+++ to.js
@@ -2,13 +2,14 @@
 function validateCode(event) {
     event.preventDefault();
     const code = prompt("ادخل كود الاشتراك");
-    const validCode = "20062000";
+    const validCode = "172018";
 
     if (code === validCode) {
         localStorage.setItem('isLoggedIn', 'true');
-        window.location.href = 'https://drive.google.com/drive/folders/1SWKnnhJY0w-lkTyvScTCYTvLo6cM5X87';
+        window.location.href = 'https://drive.google.com/drive/folders/1D8Ij-Q_exWqaugeI09WNqluZL_Otrmp7';
     } else {
         alert('كود غير صحيح');
     }
     return false;
 }
+
