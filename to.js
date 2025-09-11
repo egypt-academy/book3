4890f08a27aea8d47197b884d91c0cb0becbe17f
egypt-academy <egyptacademy11@gmail.com>
2025-09-12 01:55:39 +0300

    Update to.js

--- to.js
+++ to.js
@@ -1,7 +1,7 @@
 
 function validateCode(event) {
     event.preventDefault();
-    const code = prompt("ادخل كود الاشتراك");
+    const code = document.getElementById('code').value;
     const validCode = "172018";
 
     if (code === validCode) {
@@ -12,4 +12,5 @@ function validateCode(event) {
     }
     return false;
 }
+
 
